888b5125ebb77d707c61bbedb3b79c7e564d8385
RealPinkRabbit <sdg0896@gmail.com>
2023-10-01 01:30:34 +0900

    스타터팩 장착

---
+++ starter.py
@@ -0,0 +1,43 @@
+# 모듈 인포트
+from pico2d import *
+import random
+
+# 캔버스 오픈
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+
+# 리소스 파일 불러오기
+TUK_ground = load_image('TUK_GROUND.png')
+character = load_image('animation_sheet.png')
+hand_arrow = load_image('hand_arrow.png')
+
+# 키보드 입력 감지 함수
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+# 전역변수 선언 및 초기화
+running = True
+x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+frame = 0
+
+# 커서 숨기기
+hide_cursor()
+
+# 메인함수
+while running:
+    clear_canvas()
+    handle_events()
+    TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    update_canvas()
+    frame = (frame + 1) % 8
+    delay(0.05)
+
+# 캔버스 닫기
+close_canvas()
